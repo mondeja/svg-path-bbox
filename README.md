4aac4ccb4b64aed6d7477c2daf78050151c1d077
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-05-20 18:48:50 +0200

    Continue with testing.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # svg-path-bbox
 
-[![NPM version](https://img.shields.io/npm/v/svg-path-bbox)](https://www.npmjs.com/package/svg-path-bbox) [![Coverage Status](https://coveralls.io/repos/github/mondeja/svg-path-bbox/badge.svg?branch=master)](https://coveralls.io/github/mondeja/svg-path-bbox?branch=master) [![Node versions](https://img.shields.io/node/v/svg-path-bbox)](https://www.npmjs.com/package/svg-path-bbox) [![NPM license](https://img.shields.io/npm/l/svg-path-bbox)](https://github.com/mondeja/svg-path-bbox/blob/master/LICENSE) [![Pull requests welcome!](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](https://github.com/mondeja/svg-path-bbox/pulls) 
+[![NPM version](https://img.shields.io/npm/v/svg-path-bbox)](https://www.npmjs.com/package/svg-path-bbox) [![Tests](https://img.shields.io/travis/mondeja/svg-path-bbox?label=tests)](https://travis-ci.com/github/mondeja/svg-path-bbox) [![Coverage Status](https://coveralls.io/repos/github/mondeja/svg-path-bbox/badge.svg?branch=master)](https://coveralls.io/github/mondeja/svg-path-bbox?branch=master) [![Node versions](https://img.shields.io/node/v/svg-path-bbox)](https://www.npmjs.com/package/svg-path-bbox) [![NPM license](https://img.shields.io/npm/l/svg-path-bbox)](https://github.com/mondeja/svg-path-bbox/blob/master/LICENSE) [![Pull requests welcome!](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](https://github.com/mondeja/svg-path-bbox/pulls) 
 
 Compute bounding box of SVG paths. Pure Javascript, only [svg-path-parser](https://github.com/hughsk/svg-path-parser) as dependency.
 
--- tests/bbox.test.js
+++ tests/bbox.test.js
@@ -1,8 +1,11 @@
 'use strict';
 
-const {svgPathBbox} = require('./../src/bbox.js');
+const {
+  svgPathBbox,
+  cubicBezierCurveBbox,
+} = require('./../src/bbox.js');
 
-const svgPathBbboxCases = [
+const svgPathBbboxLinealCases = [
   // Mz
   ['M5 3z', null, null, [5, 3, 5, 3]],
   // Mz negative
@@ -12,33 +15,70 @@ const svgPathBbboxCases = [
   ['M5 3l5 0z', null, null, [5, 3, 10, 3]],
   // Mlz negative
   ['M-5 -3l10 0z', null, null, [-5, -3, 5, -3]],
+  // MLz
+  ['M5 1L6 2z', null, null, [5, 1, 6, 2]],
+  // MLz negative
+  ['M-5 -1L3 2', null, null, [-5, -1, 3, 2]],
 
   // Mmz
   ['M5 3m5 0z', null, null, [5, 3, 10, 3]],
   // Mmz negative
   ['M-5 -3m10 0z', null, null, [-5, -3, 5, -3]],
-
   // MMz
   ['M5 3M10 8z', null, null, [5, 3, 10, 8]],
   // MMz negative
   ['M-5 -3M-10 -8z', null, null, [-10, -8, -5, -3]],
 
   // Mvz
-  ['M0 5V3z', null, null, [0, 3, 0, 5]],
+  ['M1 5v3z', null, null, [1, 5, 1, 8]],
   // Mvz negative
-  ['M-5 -5V3z', null, null, [-5, -5, -5, 3]],
-
+  ['M-5 -5v3z', null, null, [-5, -5, -5, -2]],
   // MVz
   ['M1 1V5z', null, null, [1, 1, 1, 5]],
   // MVz negative
-  ['M-1 -1V-5z', null, null, [-1, -5, -1, -1]]
+  ['M-1 -1V-5z', null, null, [-1, -5, -1, -1]],
+
+  // Mhz
+  ['M1 5h3z', null, null, [1, 5, 4, 5]],
+  // Mhz negative
+  ['M-1 -5h-3z', null, null, [-4, -5, -1, -5]],
+  // MHz
+  ['M1 5H3z', null, null, [1, 5, 3, 5]],
+  // MHz negative
+  ['M-1 -5H-2z', null, null, [-2, -5, -1, -5]],
 ];
 
-describe('svgPathBbox(d, minAccuracy, maxAccuracy)', () => {
-  test.each(svgPathBbboxCases)(
-    'svgPathBbox(%p, %p, %p) => %p',
+describe('svgPathBbox(d, minAccuracy, maxAccuracy) [Lineal examples]', () => {
+  test.each(svgPathBbboxLinealCases)(
+    'svgPathBbox(%p, %p, %p)  ⇢  %p',
     (d, minAccuracy, maxAccuracy, bbox) => {
       expect(svgPathBbox(d, minAccuracy, maxAccuracy)).toEqual(bbox);
+    }
+  );
+});
+
+const svgPathBbboxCurveCases = [
+  // Mcz
+  [
+    // Relative path
+    'M1 1c1 0 1 1 0 1z', null, null, [1, 1, 1.75, 2],
+    // Absolute points with accuracy 1
+    () => { return cubicBezierCurveBbox([1, 1], [2, 1], [2, 2], [1, 2], 1); },
+  ],
+  [
+    // Absolute path
+    'M1 1C2 1 2 2 1 2z', null, null, [1, 1, 1.75, 2],
+    // Absolute points with accuracy 5
+    () => { return cubicBezierCurveBbox([1, 1], [2, 1], [2, 2], [1, 2], 5); },
+  ],
+];
+
+describe('svgPathBbox(d, minAccuracy, maxAccuracy) [Curve examples]', () => {
+  test.each(svgPathBbboxCurveCases)(
+    'svgPathBbox(%p, %p, %p)  ⇢  %p',
+    (d, minAccuracy, maxAccuracy, bbox, curveTest) => {
+      expect(svgPathBbox(d, minAccuracy, maxAccuracy)).toEqual(bbox);
+      expect(curveTest()).toEqual(bbox);
     }
   );
 });
