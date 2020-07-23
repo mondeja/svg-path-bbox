8f0cd2089b8397ceeaf21695612d9f497c928d0f
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-07-23 15:43:34 +0200

    Added some tests for 'cubicBezierXY' function.

--- tests/pol.test.js
+++ tests/pol.test.js
@@ -2,10 +2,14 @@
 
 const {
   lineXY,
+  cubicBezierXY,
 } = require('./../src/pol');
 
 const pointOnLineCases = [
   [[0, 0], [2, 0], .5, [1, 0]],
+  [[0, 0], [2, 0], 0, [0, 0]],
+  [[0, 0], [2, 0], 1, [2, 0]],
+
   [[0, 0], [10, 10], .5, [5, 5]],
   [[0, 0], [10, 10], .1, [1, 1]],
   [[0, 0], [10, 10], .7, [7, 7]],
@@ -16,6 +20,21 @@ describe('lineXY(p0, p1, t)', () => {
     'lineXY(%p, %p, %p) ⇢ %p',
     (p0, p1, t, result) => {
       expect(lineXY(p0, p1, t)).toEqual(result);
+    }
+  );
+});
+
+const pointOnCubicBezierCases = [
+  [[0, 0], [0, 10], [10, 10], [10, 0], .5, [5, 7.5]],
+  [[0, 0], [0, 10], [10, 10], [10, 0], .25, [1.5625, 5.625]],
+  [[0, 0], [0, -9], [-9, -9], [-9, 0], .5, [-4.5, -6.75]],
+];
+
+describe('cubicBezierXY(p0, p1, p2, p3, t)', () => {
+  test.each(pointOnCubicBezierCases)(
+    'cubicBezierXY(%p, %p, %p, %p, %p) ⇢ %p',
+    (p0, p1, p2, p3, t, result) => {
+      expect(cubicBezierXY(p0, p1, p2, p3, t)).toEqual(result);
     }
   );
 });
