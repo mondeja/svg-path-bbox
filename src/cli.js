74bb88a75a22908b6901236784c86165760d3ac1
Álvaro Mondéjar <mondejar1994@gmail.com>
2022-01-11 12:44:22 +0100

    Add basic options to CLI (#82)
    * Add basic options to CLI

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,20 @@
 /* eslint no-process-exit: 0 */
 /* eslint global-require: 0 */
 
+function help() {
+  const packageJson = require('../package.json');
+
+  return `${packageJson.description}
+  Output is redirected to STDOUT.
+
+Usage:
+  ${packageJson.name} [-h] [-v] [path [path ...]]
+
+Options:
+  -h, --help     Display this help text and exit.
+  -v, --version  Show this program version number (${packageJson.version}).`;
+}
+
 if (require.main === module) {
   let sliceN = 1;
   if (process.argv.indexOf(module.filename) > -1 || require('path').basename(process.argv[1]) === 'svg-path-bbox') {
@@ -13,6 +27,12 @@ if (require.main === module) {
 
   if (args.length === 0) {
     console.error('You must pass SVG paths enclosed between quotes as parameters.');
+    process.exit(1);
+  } else if (args.includes('--version') || args.includes('-v')) {
+    console.log(require('../package.json').version);
+    process.exit(1);
+  } else if (args.includes('--help') || args.includes('-h')) {
+    console.error(help());
     process.exit(1);
   }
 
--- tests/cli.test.js
+++ tests/cli.test.js
@@ -1,42 +1,55 @@
 'use strict';
 
 const path = require('path');
-const exec = require('child_process').exec;
+const {exec} = require('child_process');
+const packageJson = require('../package.json');
 
-const cli = (args, cwd) => new Promise((resolve) => {
-  exec(`node ${path.resolve(path.join('src', 'cli.js'))} ${args.join(' ')}`,
-    {cwd},
-    (error, stdout, stderr) => {
-      resolve({
-        code: error && error.code ? error.code : 0,
-        error,
-        stdout,
-        stderr
-      });
-    }
+const cli = args => new Promise((resolve) => {
+  const child = exec(
+    `node ${path.resolve(__dirname, '..', 'src', 'cli.js')}` +
+    ` ${args.join(' ')}`
   );
+
+  const stdoutChunks = [], stderrChunks = [];
+  let stdout, stderr;
+  child.on('exit', (code) => {
+    resolve({code, stdout, stderr});
+  });
+
+  child.stdout.on('data', (data) => {
+    stdoutChunks.push(data);
+  });
+  child.stdout.on('end', () => {
+    stdout = stdoutChunks.join('');
+  });
+
+  child.stderr.on('data', (data) => {
+    stderrChunks.push(data);
+  });
+  child.stderr.on('end', () => {
+    stderr = stderrChunks.join('');
+  });
 });
 
 
 const cliParameterCases = [
-  [['"M5 10c3 0 3 3 0 3z"'], 0, '5 10 7.25 13\n', null],
-  [['"M5 10c3 0 3 3 0 3z"', '"M2 8m5 5z"'], 0, '5 10 7.25 13\n2 8 7 13\n', null],
-  [[], 1, null, 'You must pass SVG paths enclosed between quotes as parameters.\n'],
+  [['"M5 10c3 0 3 3 0 3z"'], 0, '5 10 7.25 13\n', ''],
+  [['"M5 10c3 0 3 3 0 3z"', '"M2 8m5 5z"'], 0, '5 10 7.25 13\n2 8 7 13\n', ''],
+  [[], 1, '', 'You must pass SVG paths enclosed between quotes as parameters.\n'],
+  [['--version'], 1, `^${packageJson.version}\n$`, ''],
+  [['-v'], 1, `^${packageJson.version}\n$`, ''],
+  [['--help'], 1, '', `^${packageJson.description}`],
+  [['-h'], 1, '', `^${packageJson.description}`],
 ];
 
 describe('svg-path-bbox', () => {
   test.each(cliParameterCases)(
-    'svg-path-bbox %p [STDOUT: %p | STDERR: %p]',
-    (parameters, code, stdout, stderr) => {
-      cli(parameters, '.').then((proc) => {
-        expect(proc.code).toBe(code);
-        if (stdout !== null) {
-          expect(proc.stdout).toEqual(stdout);
-        }
-        if (stderr !== null) {
-          expect(proc.stderr).toEqual(stderr);
-        }
-      });
+    'svg-path-bbox %p [CODE: %p | STDOUT: %p | STDERR: %p]',
+    async (parameters, code, stdout, stderr) => {
+      const proc = await cli(parameters);
+      expect(proc.code).toBe(code);
+      expect(new RegExp(stdout).test(proc.stdout)).toEqual(true);
+      expect(new RegExp(stderr).test(proc.stderr)).toEqual(true);
     }
   );
 });
