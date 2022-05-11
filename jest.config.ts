3c1889708202fe3c46a15669402f16f32bf27b84
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-05-11 23:14:40 +0200

    Add browser end2end tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,11 @@
+const e2e = process.env.TEST_E2E,
+  unit = process.env.TEST_UNIT;
+
 export default {
-  preset: "ts-jest",
-  collectCoverage: true,
+  preset: unit ? undefined : "jest-puppeteer",
+  testMatch: [`**/tests/${e2e ? "e2e" : unit ? "!(e2e)" : "*"}.test.ts`],
+  transform: { "^.+\\.ts?$": "ts-jest" },
+  collectCoverage: !Boolean(e2e),
   coverageDirectory: "<rootDir>/tests/coverage",
   collectCoverageFrom: ["src/*.ts"],
 };
